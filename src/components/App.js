1032fad50bd0fd487f4ed6505124c4bc8053ce73
Patrick Lee <patricklee1138@live.com>
2021-07-02 13:25:09 -0700

    change render output to KegControl.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import React from "react";
 import Header from "./Header";
-import KegList from "./KegList"
+import KegControl from "./KegControl"
 
 function App(){
   return (
     <React.Fragment>
       <Header />
-      <KegList />
+      <KegControl />
     </React.Fragment>
   );
 }
