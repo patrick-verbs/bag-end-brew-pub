fa59f9cdb0f316d7f1f011429dc2dba4ffe8730d
Patrick Lee <patricklee1138@live.com>
2021-07-02 13:00:02 -0700

    refactor data to JSON

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Keg from "./Keg";
 
+const masterKegList = [
+  {
+    name: 'The Green Dragon',
+    brand: 'Against the Grain',
+    price: '5.00',
+    alcoholContent: '12.5'
+  },
+  {
+    name: 'The Hobbit: Smaug Stout',
+    brand: 'Fish Tale',
+    price: '5.00',
+    alcoholContent: '9.5'
+  }
+]
+
 function KegList(){
   return (
-    <Keg
-      name="The Green Dragon"
-      brand="Against the Grain"
-      price="5.00"
-      alcoholContent="12.5"
-    />
+    <React.Fragment>
+      <hr />
+      {masterKegList.map((keg, index) =>
+        <Keg
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          alcoholContent={keg.alcoholContent}
+          key={index}
+        />
+      )}
+    </React.Fragment>
   );
 }
 
