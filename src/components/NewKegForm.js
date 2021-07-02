e3925a2e52ae0452ba1f29903d7f4abb70227039
Patrick Lee <patricklee1138@live.com>
2021-07-02 14:11:52 -0700

    write form with event handler

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { v4 } from 'uuid';
 
-function NewKegForm(props){
+function NewKegForm() {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.brand.value);
+    console.log(event.target.price.value);
+    console.log(event.target.alcoholContent.value);
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Beer Name'
+        />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand'
+        />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price'
+        />
+        <input
+          type='number'
+          name='alcoholContent'
+          placeholder='Alcohol Content'
+        />
+        {/* <textarea
+          name='issue'
+          placeholder='Describe your issue.'
+        /> */}
+        <button type='submit'>Add Keg!</button>
+      </form>
     </React.Fragment>
   );
+
 }
 
 export default NewKegForm;
