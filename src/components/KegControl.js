eef2a6dd7c91a788f63522c1b9eb2c7118970f56
Patrick Lee <patricklee1138@live.com>
2021-07-02 13:23:32 -0700

    create with scaffold code

---
+++ src/components/KegControl.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NewKegForm from "./NewKegForm";
+import KegList from "./KegList";
+
+class KegControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default KegControl;
