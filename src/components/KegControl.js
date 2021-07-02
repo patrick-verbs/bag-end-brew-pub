b9ae92bb568934b523a5520065e99a9d71b37890
Patrick Lee <patricklee1138@live.com>
2021-07-02 13:39:29 -0700

    add click handler

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -11,16 +11,29 @@ class KegControl extends React.Component {
     };
   }
 
+  // The function to handle 'Add Keg' button click, below
+  handleClick = () => {
+    this.setState({formVisibleOnPage: true});
+  }
+
   render(){
     let currentlyVisibleState = null;
+    let addKegButton = null;
+
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm />
     } else {
       currentlyVisibleState = <KegList />
+      addKegButton = <button onClick={this.handleClick}>Add Keg</button>
+      // This doesn't render the button -- it gives it a value so it WILL render
+      // (i.e., so the value won't be 'null')
+      // That value is the HTML code for a button! One with the function, above
     }
+
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        {addKegButton}{/* Actually renders the button */}
       </React.Fragment>
     );
   }
